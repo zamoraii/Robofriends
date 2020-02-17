9db6706b596557b29d581b2e25d75420c04406de
Iker <ikerzamora@gmail.com>
2020-02-17 13:16:39 -0500

    Updates

---
+++ precache-manifest.e0038c2cc4270992ed58a94b75bfcb07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2533dc24e6e31247aab73cd39739f834",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "f208efd4cc16a41a66b9",
+    "url": "/Robofriends/static/css/main.3fb1f5b1.chunk.css"
+  },
+  {
+    "revision": "d65cd17bf9a22e02b90a",
+    "url": "/Robofriends/static/js/2.5ebca8c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Robofriends/static/js/2.5ebca8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f208efd4cc16a41a66b9",
+    "url": "/Robofriends/static/js/main.44126da2.chunk.js"
+  },
+  {
+    "revision": "9eff52c4d1627d87e058",
+    "url": "/Robofriends/static/js/runtime-main.b221b423.js"
+  },
+  {
+    "revision": "3ff4f9f021810cf54660b08ace55a697",
+    "url": "/Robofriends/static/media/HUMANOIDSTRAIGHT.3ff4f9f0.TTF"
+  }
+]);
